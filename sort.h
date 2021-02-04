5b2ce5d22f4142232242f43fa0713081c53c2c56
sebastian <sebastian.calleu@gmail.com>
2021-02-03 21:12:50 -0500

    style(betty): fixing code style.

--- sort.h
+++ sort.h
@@ -25,7 +25,7 @@ void insertion_sort_list(listint_t **);
 void selection_sort(int *, size_t);
 int sortcheck(int *);
 int countnodes(listint_t *);
-void nodeswaper(listint_t **, listint_t **, listint_t **, int );
+void nodeswaper(listint_t **, listint_t **, listint_t **, int);
 void quick_sort(int *, size_t);
 void quicsortrec(int *, int, int, int);
 int lomutopart(int *, int, int, int);
