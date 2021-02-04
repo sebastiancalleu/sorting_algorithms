34663d7a7218f72d0ddd11b79f543d98de0d6a50
sebastian <sebastian.calleu@gmail.com>
2021-02-03 22:17:31 -0500

    factor(edge): adding edge cases null and size 1.

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,6 +11,9 @@ void shell_sort(int *array, size_t size)
 	size_t interval = 1, a;
 	int tmp1, flag;
 
+	if (array == NULL || size < 2)
+		return;
+
 	while (interval < size / 3)
 		interval = interval * 3 + 1;
 
